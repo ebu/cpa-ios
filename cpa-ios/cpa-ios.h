de6b4919d2162df524f130cb82b43782e37a22c5
Samuel Défago <defagos@gmail.com>
2015-04-15 08:34:55 +0200

    Remove useless imports

--- cpa-ios/cpa-ios.h
+++ cpa-ios/cpa-ios.h
@@ -4,10 +4,6 @@
 //  Licence information is available from the LICENCE file.
 //
 
-#import <Foundation/Foundation.h>
-#import <Security/Security.h>
-#import <UIKit/UIKit.h>
-
 #import <cpa-ios/CPAErrors.h>
 #import <cpa-ios/CPAProvider.h>
 #import <cpa-ios/CPAToken.h>
