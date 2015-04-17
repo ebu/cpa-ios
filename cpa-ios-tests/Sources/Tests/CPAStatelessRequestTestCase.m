1e916b5cb89a0317812f3134bda2f69dadf07aad
Samuel Défago <defagos@gmail.com>
2015-04-17 11:53:23 +0200

    Add test bundle

---
+++ cpa-ios-tests/Sources/Tests/CPAStatelessRequestTestCase.m
@@ -0,0 +1,21 @@
+//
+//  Copyright (c) European Broadcasting Union. All rights reserved.
+//
+//  Licence information is available from the LICENCE file.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CPAStatelessRequestTestCase : XCTestCase
+
+@end
+
+@implementation CPAStatelessRequestTestCase
+
+- (void)testExample
+{
+    XCTAssert(YES, @"OK");
+}
+
+@end
