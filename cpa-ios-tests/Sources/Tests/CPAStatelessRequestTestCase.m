566d10ec1f43e02698c17f8fe9530f27424eaead
Samuel Défago <defagos@gmail.com>
2015-04-28 06:57:09 +0200

    Write registration test

--- cpa-ios-tests/Sources/Tests/CPAStatelessRequestTestCase.m
+++ cpa-ios-tests/Sources/Tests/CPAStatelessRequestTestCase.m
@@ -32,20 +32,21 @@ static NSTimeInterval kConnectionTimeOut = 60;
 
 #pragma mark Tests
 
-- (void)testExample
+- (void)testRegisterClient
 {
     XCTestExpectation *expectation = [self expectationWithDescription:@"Client registration request"];
     NSURL *authorizationProviderURL = [NSURL URLWithString:@"https://cpa.rts.ch"];
     
     [CPAStatelessRequest registerClientWithAuthorizationProviderURL:authorizationProviderURL clientName:@"iOS Test" softwareIdentifier:@"ch.ebu.ios_test" softwareVersion:@"0.1" completionBlock:^(NSString *clientIdentifier, NSString *clientSecret, NSError *error) {
-        // ...
-        
+        XCTAssertEqualObjects(clientIdentifier, @"407");
+        XCTAssertEqualObjects(clientSecret, @"f9f1c336a59219e05a59eecb40eb49eb");
+        XCTAssertNil(error);
         [expectation fulfill];
     }];
     
     // TODO: Provide a wrapper which takes a cassette name as parameter and load / release it
     [self waitForExpectationsWithTimeout:kConnectionTimeOut handler:^(NSError *error) {
-        // ...
+        XCTAssertNil(error);
     }];
 }
 
