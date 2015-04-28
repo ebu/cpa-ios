61926be81e9045386fdcdabcf2eb9f95706611a6
Samuel Défago <defagos@gmail.com>
2015-04-28 06:27:30 +0200

    Test headers when resolving stubs

--- cpa-ios-tests/Sources/Helpers/HTTPStub.m
+++ cpa-ios-tests/Sources/Helpers/HTTPStub.m
@@ -90,12 +90,34 @@
 
 - (BOOL)matchesRequest:(NSURLRequest *)request
 {
-    NSDictionary *requestHTTPBodyDictionary = [NSJSONSerialization JSONObjectWithData:request.HTTPBody options:0 error:NULL];
-    NSDictionary *HTTPBodyDictionary = [NSJSONSerialization JSONObjectWithData:self.requestStubFile.bodyData options:0 error:NULL];
-    
     return HTTPMethodForName(request.HTTPMethod) == self.requestStubFile.method
         && [request.URL.path isEqualToString:self.requestStubFile.path]
-        && [requestHTTPBodyDictionary isEqualToDictionary:HTTPBodyDictionary];
+        && [self matchesBodyOfRequest:request]
+        && [self matchesHeadersOFRequest:request];
+}
+
+- (BOOL)matchesHeadersOFRequest:(NSURLRequest *)request
+{
+    static NSArray *s_ignoredFields;
+    static dispatch_once_t s_onceToken;
+    dispatch_once(&s_onceToken, ^{
+        s_ignoredFields = @[@"Cookie", @"Connection", @"User-Agent", @"Content-Length", @"Host"];
+    });
+    
+    NSMutableDictionary *requestHeaders = [NSMutableDictionary dictionaryWithDictionary:request.allHTTPHeaderFields];
+    [requestHeaders removeObjectsForKeys:s_ignoredFields];
+    
+    NSMutableDictionary *headers = [NSMutableDictionary dictionaryWithDictionary:self.requestStubFile.headers];
+    [headers removeObjectsForKeys:s_ignoredFields];
+    
+    return [requestHeaders isEqualToDictionary:headers];
+}
+
+- (BOOL)matchesBodyOfRequest:(NSURLRequest *)request
+{
+    NSDictionary *requestBodyDictionary = [NSJSONSerialization JSONObjectWithData:request.HTTPBody options:0 error:NULL];
+    NSDictionary *bodyDictionary = [NSJSONSerialization JSONObjectWithData:self.requestStubFile.bodyData options:0 error:NULL];
+    return [requestBodyDictionary isEqualToDictionary:bodyDictionary];
 }
 
 - (OHHTTPStubsResponse *)response
